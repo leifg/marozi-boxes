3e766578be1bd08b5a0512bcd659e8e19fa03a7f
Leif Gensert <leif@gensert.de>
2013-01-20 11:16:55 +0100

    remove base role, add script to replace it from template

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ gem instal librarian
 gem install vagrant
 rbenv rehash
 librarian-chef install --path chef/cookbooks
+
+for f in chef/roles/*.example
+do
+  new_filename="${f%.*}"
+  cp -f $f $new_filename
+done
